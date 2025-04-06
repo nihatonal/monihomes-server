c3e4812fc2b5164b3f05925693abad6f9e7ec79f
nihatonal <onalnihat1986@gmail.com>
2025-04-06 14:10:50 +0300

    updated GOOGLE API

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -2,7 +2,7 @@ const https = require('https');
 
 
 const get_reviews = async (req, res, next) => {
-  const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${process.env.PLACE_ID}&key=${process.env.API_KEY}`;
+  const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${process.env.PLACE_ID}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
 
   https.get(url, ress => {
     let data = '';
